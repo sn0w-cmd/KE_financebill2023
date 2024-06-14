c6677c7d0a734aa3828e061f30f3c28739b1b147
alex.muturi <alex.muturi@student.moringaschool.com>
2024-06-14 11:59:24 +0300

    fix df view

--- streamlit-deploy.py
+++ streamlit-deploy.py
@@ -20,13 +20,10 @@ st.sidebar.info(
     
     "IS ABSENCE = YES?"
 )
-st.title("How did your MP Vote on the Finance Bill 2024?",)
+st.title("How did your MP Vote on the Finance Bill 2024?")
 
 
 
-
-
-# with st.form("my_form"):
 input_query = st.text_input("Enter the name of your constituency (or county for woman rep)")
 submitted = st.button("Search")
 if submitted:
@@ -35,32 +32,7 @@ if submitted:
 
     st.dataframe(data=updated_df, column_order=("Other Names", "Vote", "Constituency"), hide_index=True)
 
-# # Upload image through Streamlit
-# # input_query = st.text_input("Enter the name of your constituency")
-
-# # uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-# normalized_counts = pd.DataFrame(df['Vote'].value_counts(ascending=False))
-
-# # Plot bar chart
-
-# st.bar_chart(normalized_counts, x=None, y='Vote')
-
 
 
 st.subheader("Full records")
 st.dataframe(data=df)
-
-
-# # Upload image through Streamlit
-# input_query = st.text_input("Enter the name of your constituency")
-# # uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-
-
-
-# if input_query is not None:
-#     # Display the uploaded image
-#     input_query = input_query.capitalize()
-#     # df[df['Constituency'] == input_query]
-#     st.write(df[df['Constituency'] == input_query])
-
-
